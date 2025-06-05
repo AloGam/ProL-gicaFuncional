738558efa4dae034ce2b3e341cce2a0cc4ffa0d5
AloGam <alegamhu@gmail.com>
2025-06-05 05:36:27 -0600

    AA4.2

--- AA4.1-/SistemRiego.py
+++ AA4.1-/SistemRiego.py
@@ -1,7 +1,7 @@
 # Estado de sensores
 humedad_suelo = 'baja'
 temperatura = 30
-hora = 20
+hora = 14
 prob_lluvia = False
 # Función para verificar si es la hora adecuada para regar
 def hora_adecuada():
